08f0ef6135b5923d9222e3b82b4740587b6fb591
Maeva DTC <delruemaeva@orange.fr>
2024-08-17 21:34:23 +0200

    first repo of my very first little full-stack project

---
+++ index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.API_KEY,
+});
+
+const sentFrom = new Sender(`Maeva@${process.env.DOMAIN}`, "Maeva Delrue");
+
+app.get("/", (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json("Bienvenue sur notre back pour form de contact");
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/contact", async (req, res) => {
+  try {
+    //   Le console.log de req.body nous affiche les données qui ont été rentrées dans les inputs (dans le formulaire frontend)
+    console.log("body =>", req.body);
+    const recipients = [
+      new Recipient(
+        req.body.email,
+        `${req.body.firstName} ${req.body.lastName}`
+      ),
+    ];
+
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setSubject("Demande de renseignements")
+      .setHtml("<strong>" + req.body.message + "</strong>")
+      .setText(req.body.message);
+
+    const result = await mailerSend.email.send(emailParams);
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started 📧");
+});
---
+++ main.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //   const header = document.querySelector("header");
+  const connect = document.querySelector(".connect");
+  const modal = document.querySelector(".modal");
+  const body = document.querySelector("body");
+  const closeForm = document.querySelector(".fa-solid.fa-xmark");
+  const form = document.querySelector("#contact");
+
+  // Add bottom border on header when scrolling down
+  window.addEventListener("scroll", () => {
+    if (window.scrollY !== 0) {
+      header.classList.add("bordered");
+    } else {
+      header.classList.remove("bordered");
+    }
+  });
+
+  // When the button "connectez-vous" is clicked
+  connect.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    body.classList.add("disable");
+  });
+
+  // When the cross on the modal is clicked
+  closeForm.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    body.classList.remove("disable");
+  });
+  ////
+  // FRONT PART
+  ////
+  form.addEventListener("submit", async (event) => {
+    // console.log(event); affiche la cible de l'évenemen (donc ici form) - et pas le BUTTON -
+    event.preventDefault();
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      message: message,
+    };
+    try {
+      const response = await axios.post("http://localhost:3000/contact", data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  });
+  //   console.log(form); // affiche le formulaire
+
+  //   const response = await axios.get("http://localhost:3000/");
+  //   console.log("response =>", response.data); // response => Bienvenue sur notre back pour form de contact
+});
+
+// récupérer les données à la soumission du formulaire
